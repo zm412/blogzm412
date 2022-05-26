9db72d18715739e6d40f10718ca08e03ceb40349
zm412 <zm412412@gmail.com>
2022-05-26 01:52:08 -0400

    added readme

--- expressapp/db/models/post.model.js
+++ expressapp/db/models/post.model.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, Sequelize) => {
   const Post = sequelize.define("post", {
-    post_text: Sequelize.STRING,
+    post_text: Sequelize.TEXT,
     file_url: Sequelize.STRING,
   });
 
